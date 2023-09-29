25054e86a972417afac368d4efe51bd3b03f8c25
Jesus Barboza <jesus.barboza@samishop.pe>
2023-09-29 16:24:44 -0500

    CHORE: set real port

--- server/index.js
+++ server/index.js
@@ -3,7 +3,7 @@ require("dotenv").config();
 const config = require('./src/config');
 
 const app = require("./src/app")
-const port = config.PORT
+const port = process.env.PORT || 3000
 
 app.listen(port, () => { 
     console.log("Running on port", port);
--- server/src/config.js
+++ server/src/config.js
@@ -1,7 +1,5 @@
 const isProduction = process.env.NODE_ENV !== "dev";
 const DATABASE_URL = isProduction ? process.env.MONGO_URL : process.env.MONGODB_URI
-const PORT = isProduction ? process.env.MONGOPORT : process.env.PORT 
 module.exports = {
-  DATABASE_URL,
-  PORT
+  DATABASE_URL
 };
