753b3c79a436d6fb905f6680dd866a2b7b6d335e
Jesus Barboza <jesus.barboza@samishop.pe>
2023-09-29 16:05:34 -0500

    CHORE: set real port

--- server/src/config.js
+++ server/src/config.js
@@ -1,6 +1,6 @@
 const isProduction = process.env.NODE_ENV !== "dev";
 const DATABASE_URL = isProduction ? process.env.MONGO_URL : process.env.MONGODB_URI
-const PORT = isProduction ? process.env.MONGODB_PORT : process.env.PORT 
+const PORT = isProduction ? process.env.MONGOPORT : process.env.PORT 
 module.exports = {
   DATABASE_URL,
   PORT
